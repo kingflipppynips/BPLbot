e96e13e32599ea1bf941817248b49f0433c9ccf5
KingFlippynips <flippynips@blanket.systems>
2017-08-05 20:21:45 -0400

    Added status module and some simple status returns

---
+++ bot_modules/status.js
@@ -0,0 +1,29 @@
+var statusModules = {};
+
+module.exports = {
+
+	setStatusModules: function(modules) {
+		statusModules = modules;
+	},
+
+	// return true if we handle channel
+	channelFilter: function(channel) {
+		return true;
+	},
+
+	onCommand: async function(client, message, command, args) {
+
+		if (!message.member.roles.some(r => ["Moderator"].includes(r.name)))
+			return;
+
+		if (command === "status") {
+      var statusMsg = "";
+			Object.values(statusModules).forEach(function(module) {
+				if (module.getStatus) {
+					statusMsg += module.getStatus() + "\n";
+				}
+			});
+      message.channel.send("```" + statusMsg + "```");
+		}
+	}
+}
